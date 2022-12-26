bfb9aba21b2e39a40a735e286689706647d149e3
GilValdes129 <70418863+GilValdes129@users.noreply.github.com>
2022-12-26 15:46:20 -0600

    Display of player score with the use of arrays

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,18 +4,25 @@ var submitBtn = document.querySelector("#SubmitBtn")
 var score = localStorage.getItem("score")
 var scorecontainer = document.querySelector("#ScoreContainer")
 var scoreList = document.querySelector("#ScoreList")
+//var scoretracker = document.querySelector("#prueba")
 
 var players = [
-    {
-        player: localStorage.getItem("name"),
-        score: score
-    }
+ 
 ]
+players.sort(function(a, b){
+    b.score - a.score
+})
 
 
-submitBtn.addEventListener("click", displaylist);
 
-if(score > 0){
+localStorage.setItem("players", JSON.stringify(players))
+
+submitBtn.addEventListener("click", createuser);
+
+
+
+
+if(score >= 0){
     scorecontainer.classList.remove("hide")
 }
 
@@ -27,13 +34,30 @@ function addName(){
     return
 }
 
-function displaylist(){
+function createuser(){
     addName()
-    var ListItem = document.createElement("li")
-    ListItem.innerText = `${players[0].player} have a score of ${players[0].score}`
-    scoreList.appendChild(ListItem)
+    var player = {
+        player: localStorage.getItem("name"),
+        score: localStorage.getItem("score"),
+    };
+    players.push(player)
+    localStorage.setItem("players", JSON.stringify(players))
+    console.log(players)
+    displaylist()
+}
+
+function displaylist(){
+    var arr = JSON.parse(localStorage.getItem("players"));
+    scoreList.innerHTML = ""
+    for (var i=0; i < arr.length; i++){
+        var ListItem = document.createElement("li")
+        ListItem.innerText = `${players[i].player} have a score of ${players[i].score}`
+        scoreList.appendChild(ListItem)
+        
+    }
+    
+    console.log(JSON.parse(localStorage.getItem("players")))
 }
 
 
-var prueba = localStorage.getItem("name")
-console.log(players)
+
